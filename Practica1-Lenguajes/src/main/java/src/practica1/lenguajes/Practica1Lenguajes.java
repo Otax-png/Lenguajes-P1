86460f89ae47c23594fc027684d0f7c16884f023
Otax-png <taxturnil201@gmail.com>
2023-08-04 23:19:18 -0600

    Primer Commit

---
+++ Practica1-Lenguajes/src/main/java/src/practica1/lenguajes/Practica1Lenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package src.practica1.lenguajes;
+
+/**
+ *
+ * @author osvaldo
+ */
+public class Practica1Lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
